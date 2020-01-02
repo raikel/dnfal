f7f36a49aaabb512ac85292edeab1c6da493f6d0
raikel <raikelbl@gmail.com>
2020-01-02 11:55:24 -0600

    Project updated

---
+++ getmodels.sh
@@ -0,0 +1,4 @@
+mkdir -p models
+wget -O models/weights_detector.pth "https://www.dropbox.com/s/z4l7rasz0ydjt9f/weights_detector.pth?dl=0"
+wget -O models/weights_encoder.pth "https://www.dropbox.com/s/6a3eztge2ghci3q/weights_encoder.pth?dl=0"
+wget -O models/weights_marker.npy "https://www.dropbox.com/s/2ne3ronmdqgxsca/weights_marker.npy?dl=0"
